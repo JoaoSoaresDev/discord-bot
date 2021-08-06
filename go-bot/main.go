a19a59095ff7176294f656cbcf304677f343ff8d
Joao Soares <62054293+JoaoSoaresDev@users.noreply.github.com>
2021-08-06 18:11:44 +0800

    discord-bot first commit

--- go-bot/main.go
+++ go-bot/main.go
@@ -1,31 +1,24 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
-	_ "github.com/lib/pq"
 )
 
 const token string = "ODcyNzMwOTg1OTcwNjYzNDU0.YQuIEQ.7cnjUQVo-vdYOKyyYHM2bmAvzG0"
 
 var BotID string
 
-var db *sql.DB
-
-var today time.Weekday
-
-var people []string
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "Maquina01@"
-	dbname   = "postgres"
-)
+var day_people = [5][]string{
+	{"Alicia", "Alyssa", "John", "Oli"},
+	{"Alicia", "Alyssa", "Arjun", "John", "Oli"},
+	{"Alicia", "Alyssa", "Arjun", "John"},
+	{"Alicia", "Alyssa", "John", "Oli", "Sanam"},
+	{"Alicia", "Alyssa", "Arjun", "Oli", "Sanam"},
+}
 
 func main() {
 
@@ -55,72 +48,27 @@ func main() {
 
 	fmt.Println("Bot is running!")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	//defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
-
 	<-make(chan struct{})
 	return
 }
 
-/*func retrievePeople(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	err := db.QueryRow("SELECT people FROM weekly_availability WHERE week_days=?", today).Scan(&people)
-
-	if err != nil {
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println(people)
-}*/
-
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	if m.Content == "!today" {
+	if m.Content == "/today" {
 		today := time.Now().Weekday()
 
-		//http.HandleFunc("/retrieve", retrievePeople)
+		var all_people string
 
-		userSql := "SELECT people FROM weekly_availability WHERE week_days=?"
-
-		db.QueryRow(userSql, today.String()).Scan(&people)
-
-		var peopleFull string = ""
-
-		for _, str := range people {
-			peopleFull += str + " "
+		for _, str := range day_people[int(today-1)] {
+			all_people += str + ", "
 		}
 
-		_, _ = s.ChannelMessageSend(m.ChannelID, today.String()+" schedule:\n "+peopleFull)
+		all_people = strings.TrimSuffix(all_people, ", ")
 
-		//http.ListenAndServe(":8080", nil)
-
-		//defer db.Close()
-
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Who will be in the office today:\n"+today.String()+":\n"+all_people)
 	}
 
 	if m.Author.ID == BotID {
 		return
 	}
-
-	fmt.Println(m.Content)
 }
